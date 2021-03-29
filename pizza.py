a1c0d81692622b661fb4116d9406f3fa98317bbd
Ahmadreza <mamalrms1906@gmail.com>
2021-03-29 18:25:20 +0430

    barname tabdil be class chod

--- pizza.py
+++ pizza.py
@@ -6,7 +6,8 @@ import os
 from datetime import date
 
 class doakbi():
-    def bing(url):            
+    def bing(url):
+        
         try:
             matn=requests.get(url)    
         except HTTPError as er:
@@ -16,15 +17,15 @@ class doakbi():
         pardazesh_1=BeautifulSoup(matn.text,'html.parser')
         pardazesh_2=pardazesh_1.head
         pardazesh_3=pardazesh_2.select('link',{'rel':'preload'})[1]
-        pardazesh_4=pardazesh_3.attrs['href']   
-        newpath = r'C:\Users\Rajabian\Desktop' 
-        if not os.path.exists(newpath):
-            os.makedirs(newpath)
+        pardazesh_4=pardazesh_3.attrs['href']
         global date    
         today = date.today()
         date = today.strftime("%b-%d-%Y")
+        newpath = 'C:\\Users\\Rajabian\\Desktop\\maml\\'+date 
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
         response=requests.get(url+pardazesh_4)
-        with open (date +'m'+".jpg",'wb')as file:
+        with open (newpath+'mamal'+".jpg",'wb')as file:
             return file.write(response.content)
             
    # print(bing('https://www.bing.com'))   
