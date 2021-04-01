32efb45d09f616b72bfa9395315391b752f7bcb5
Ahmadreza <mamalrms1906@gmail.com>
2021-04-01 17:15:57 +0430

    start for all OS

--- pizza.py
+++ pizza.py
@@ -8,24 +8,26 @@ import os
 
 class doakbi():
     def bing(url):        
-        try:
-            matn=requests.get(url)    
-        except HTTPError as er:
+         try:
+             matn=requests.get(url)    
+         except HTTPError as er:
             print('ops! HTTPError')
-        except URLError as er:
+         except URLError as er:
             print('ops!! URLError')
-        pardazesh_1=BeautifulSoup(matn.text,'html.parser')
-        pardazesh_2=pardazesh_1.head
-        pardazesh_3=pardazesh_2.select('link',{'rel':'preload'})[1]
-        pardazesh_4=pardazesh_3.attrs['href']
-        global date    
-        today = date.today()
-        date = today.strftime("%b-%d-%Y")
-        newpath = 'C:\\Users\\Rajabian\\Desktop\\maml\\'+date 
-        if not os.path.exists(newpath):
+         pardazesh_1=BeautifulSoup(matn.text,'html.parser')
+         pardazesh_2=pardazesh_1.head
+         pardazesh_3=pardazesh_2.select('link',{'rel':'preload'})[1]
+         pardazesh_4=pardazesh_3.attrs['href']
+         global date    
+         today = date.today()
+         date = today.strftime("%b-%d-%Y")
+         my = os.path.sep+ "mamalkhan" + os.path.sep + date
+         newpath = my 
+         print(my)
+         if not os.path.exists(newpath):
             os.makedirs(newpath)
-        response=requests.get(url+pardazesh_4)
-        with open (newpath+'mamal'+".jpg",'wb')as file:
-            return file.write(response.content)            
-   # print(bing('https://www.bing.com'))   
+         response=requests.get(url+pardazesh_4)
+         with open (newpath+".jpg",'wb')as file:
+            return file.write(response.content)
+#print(bing('https://www.bing.com'))   
 
