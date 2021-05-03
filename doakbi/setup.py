b0cb53fea0e25d8bcd43885bf5360f116ae8cb4f
Ahmadreza <mamalrms1906@gmail.com>
2021-05-03 14:35:24 +0430

    for pack 3

---
+++ doakbi/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='doakbi',
+    version='0.0.1',
+    description='my first',
+    url='https://github.com/ahmadrezasalar/doakbi.git',
+    author='ahmadrezasalar',
+    author_email='mamalrms1906@gmail.com',
+    license='unlicense',
+    packages=['doakbi'],
+    zip_safe=False
+    
+    )
